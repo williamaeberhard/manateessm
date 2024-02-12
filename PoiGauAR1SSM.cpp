007dadafe2635ffdcd8cf499c14654b2043dc5ce
William Aeberhard <william.aeberhard@gmail.com>
2024-02-12 17:03:38 +0100

    v0.1 not done with fit and cpp

---
+++ PoiGauAR1SSM.cpp
@@ -0,0 +1,122 @@
+// Poisson-Gaussian SSM, stationary AR(1) states | v0.1
+// allow for NAs in obs vec, obsind id them as 0, but states defined for all t
+//   Y_t|X_t indep~ Poi with E[Y_t|X_t] = exp(beta0 + z*beta + X_t), t=1,...,n
+//   X_t = phi*X_{t-1} + eps_t, where eps_t iid~ N(0,sigma^2), t=2,...,n
+//   X_1 ~ N(0,sigma^2/(1-phi^2)), stationary dist since assuming |phi|<1
+
+#include <TMB.hpp>
+
+template <class Type> 
+Type sqr(Type x){
+	return x*x;
+}
+
+// parameter transformation, R -> (-1,+1)
+template <class Type>
+Type bound11(Type x){
+	// return (Type(1.0)-exp(-x))/(Type(1.0)+exp(-x));
+	return Type(2.0)/(Type(1.0)+exp(-x)) - Type(1.0);
+}
+
+template<class Type>
+Type objective_function<Type>::operator() () {
+
+	//--------------------------------------------------------------------------
+	// Inputs
+	//--------------------------------------------------------------------------
+
+	// Data
+	DATA_VECTOR(obs); // counts vector, dim n
+	// ^ incl NA entries from padding in R, obsind sorts them
+	DATA_IVECTOR(obsind); // 1 if count available, 0 if missing, dim n
+	DATA_VECTOR(logoffset); // measure of effort, coef 1 in lin pred, dim n
+	DATA_MATRIX(Zmat); // matrix of det covariates, dim (n x p)
+	DATA_MATRIX(Zmattemp); // piecewise temp lin fx, dim (n x 4)
+
+	// Parameters
+	PARAMETER_VECTOR(beta); // det fx, incl intercept, dim p
+	PARAMETER_VECTOR(betatemp); // temp piecewise lin fx, dim 2
+	PARAMETER(tphi); // transformed AR(1) coef
+	PARAMETER(logsigma); // sd of Gaussian proc error for AR(1) on X
+
+	// Random effects
+	PARAMETER_VECTOR(X); // unobserved states AR(1), dim n
+
+	// Misc
+	DATA_SCALAR(tempthresh); // temp value piecewise lin fx junction
+
+
+	//--------------------------------------------------------------------------
+	// Setup, procedures and init
+	//--------------------------------------------------------------------------
+
+	int n = obs.size(); // t = 1, ..., n
+	
+	Type phi = bound11(tphi);
+	Type sigma = exp(logsigma);
+
+	vector<Type> betatempvec(4); // vector of all pw lin fx coeff, incl constr
+	betatempvec(0) = Type(0.0);
+	// ^ intercept temp <= thresh, constrained to overall intercept
+	betatempvec(1) = betatemp(0); // slope temp <= thresh
+	betatempvec(2) = tempthresh*(betatemp(0)-betatemp(1));
+	// ^ intercept temp > thresh, constrained for continuity at thresh
+	betatempvec(3) = betatemp(1); // slope temp > thresh
+
+	vector<Type> zb = Zmat*beta + Zmattemp*betatempvec;
+	// ^ all deterministic effects as two linear combinations, dim n
+
+	Type nll = 0.0; // init neg loglik
+
+
+// here!!! finish double-check below
+
+
+
+	//--------------------------------------------------------------------------
+	// Random effects
+	//--------------------------------------------------------------------------
+
+	Type inisd = sigma/sqrt(Type(1.0) - sqr(phi)); // AR(1) stationary sd
+	nll -= dnorm(X(0), Type(0.0), inisd, true);
+	// ^ stationary dist for ini states
+
+	for (int i=1; i<n; i++){
+		Type Xcondexpec = phi*X(i-1); // AR(1) cond expec
+		nll -= dnorm(X(i), Xcondexpec, sigma, true); // Gaussian loglik
+	}
+	
+
+	//--------------------------------------------------------------------------
+	// Observation equations
+	//--------------------------------------------------------------------------
+
+	vector<Type> fitted(n); // Poisson cond expec, dim n
+	for (int i=0; i<n; i++){
+		fitted(i) = exp(logoffset(i) + zb(i) + X(i)); // Poi cond expec
+		// ^ fitted = exp(linear predictor)*offset
+		if (obsind(i)==1){ // lkhd contrib only when obs available
+			nll -= dpois(obs(i), fitted(i), true);
+		}
+	}
+
+
+	//--------------------------------------------------------------------------
+	// Outputs
+	//--------------------------------------------------------------------------
+
+	REPORT(beta);
+	REPORT(betatempvec);
+	REPORT(phi);
+	REPORT(sigma);
+	REPORT(inisd);
+
+	REPORT(zb); // detfx linear combination of covariate effects
+	REPORT(X); // predicted states
+	REPORT(fitted); // Poisson cond expec, fitted values on response scale
+
+	ADREPORT(beta); // for detfx se, needed for z-test stepwise var sel
+	ADREPORT(betatemp); // for detfx se, needed for z-test stepwise var sel
+
+	return nll;
+}
--- manateessm_main.r
+++ manateessm_main.r
@@ -2,6 +2,10 @@
 #### Fit Poisson-Gaussian SSM on simulated data ####
 #///////////////////////////////////////////////////
 
+# rm(list=ls()) # clear global environment
+
+# setwd('.') # set to cloned manateessm GitHub repository
+
 
 ### // load libraries ----
 
@@ -10,22 +14,74 @@ library(splines) # for B-splines bs()
 
 # TODO: adapt and simplify scripts for illustration with simulated data
 
+
+### // compile and load model C++ script ----
+
+# TODO
+
 # # system.time(compile("PoiGauAR1SSM_alt.cpp")) # run only once
 # dyn.load(dynlib("PoiGauAR1SSM_alt")) # run for every new session
 # # ^ v0.3-1 forked from v0.3: piecewise linear temp fx separate from Zmat
-# 
 
 
+### // load data from supplied txt file ----
+
+dat <- read.table('manateessm_data.txt',sep=',',header=T)
+str(dat) # 100 rows = 100 months
+# ^ month imported as character string by default
+dat$ts <- as.Date(paste0(dat$month,'-01')) # fake day 01 for format YYYY-MM-DD
+str(dat) # ts = time stamp
+
+nT <- dim(dat)[1] # total number of time points = 100
 
 
+### // pre-processing: linear time trend ----
+
+cov.timetrend <- (0:(nT-1))/(nT-1)
+# ^ whole time window seen as [0,1], with 0 = first month and 1 = last month
 
 
+### // pre-processing: seasonality as two seasons, winter vs. others ----
+
+ts.month <- as.integer(substr(dat$ts,6,7)) # month as integer, 1-12
+# ^ extracts only month from ts
+table(ts.month)
+# ^ each month appears 8-9 times
+
+cov.seas <- rep(1L,nT) # ini with 1s, 2 seasons dummy code, winter vs. others
+for (i in 1:nT){
+	if (ts.month[i]%in%c(12,1,2)){ # 12=Dec, 1=Jan, 2=Feb
+		cov.seas[i] <- 0L # winter months coded as 0
+	}
+}
+cbind(ts.month, cov.seas)
+# ^ winter months set as 0 => winter is the reference in the intercept and the
+#   coefficient associated with cov.seas represents the average difference
+#   between Mar-Nov and {Jan, Feb, Dec}.
 
 
+### // pre-processing: piecewise linear effect of temperature ----
+
+thresh.tem <- 24.7
+# ^ hard-coded threshold for the two linear pieces
+
+tem.01 <- as.numeric(dat$temperature > thresh.tem)
+# ^ months with temperature <= threshold coded as 0, months with temperature >
+#   threshold coded as 1
+
+Z.tem <- cbind(
+	1-tem.01,                   # intercept tem <= threshold
+	dat$temperature*(1-tem.01), # slope tem <= threshold
+	tem.01,                     # intercept tem > threshold
+	dat$temperature*tem.01      # slope tem > threshold
+)
+summary(Z.tem)
+head(Z.tem)
+tail(Z.tem)
+# ^ all intercept and slopes, constraints on coefficients hard-coded in cpp
 
 
+### // setup objectsn for fit ----
 
-
-
-
+# here!!!
 
